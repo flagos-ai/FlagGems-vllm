3fbda41edefa807922416ed9e163db76a4885d00
luojie <hellojack163@gmail.com>
2026-08-11 17:38:01 +0800

    [KMCompiler] added per_token_group_quant_fp8 for hygon backend

--- src/flaggems_vllm/runtime/backend/_hygon/ops/__init__.py
+++ src/flaggems_vllm/runtime/backend/_hygon/ops/__init__.py
@@ -12,19 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 from .fused_moe import (  # noqa: F401
     fused_experts_impl,
     inplace_fused_experts,
     outplace_fused_experts,
 )
+from .per_token_group_quant_fp8 import SUPPORTED_FP8_DTYPE, per_token_group_quant_fp8
 from .scaled_int8_quant import scaled_int8_quant
 from .triton_scaled_mm import triton_scaled_mm
 
 __all__ = [
+    "SUPPORTED_FP8_DTYPE",
     "fused_experts_impl",
     "inplace_fused_experts",
     "outplace_fused_experts",
+    "per_token_group_quant_fp8",
     "scaled_int8_quant",
     "triton_scaled_mm",
 ]
