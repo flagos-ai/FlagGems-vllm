4673b74cb3091d5f0a04c5e61f13a1ba27fac28e
monellz <rxzhong@outlook.com>
2026-09-11 14:22:43 +0800

    Add fp8_einsum with tests and vLLM benchmarks

--- src/flaggems_vllm/ops/__init__.py
+++ src/flaggems_vllm/ops/__init__.py
@@ -50,6 +50,7 @@ from flaggems_vllm.ops.attention import (
 from flaggems_vllm.ops.flash_mla import flash_mla
 from flaggems_vllm.ops.flash_mla_with_kvcache import flash_mla_with_kvcache
 from flaggems_vllm.ops.flashmla_sparse import flash_mla_sparse_fwd
+from flaggems_vllm.ops.fp8_einsum import fp8_einsum
 from flaggems_vllm.ops.fp8_fp4_mqa_logits import fp8_fp4_mqa_logits
 from flaggems_vllm.ops.fp8_fp4_paged_mqa_logits import fp8_fp4_paged_mqa_logits
 from flaggems_vllm.ops.fused_add_rms_norm import fused_add_rms_norm
@@ -174,6 +175,7 @@ __all__ = [
     "flash_mla",
     "flash_mla_sparse_fwd",
     "flash_mla_with_kvcache",
+    "fp8_einsum",
     "fp8_fp4_mqa_logits",
     "fp8_fp4_paged_mqa_logits",
     "fused_add_rms_norm",
