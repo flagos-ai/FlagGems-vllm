d55c9215f8623927394e2c642ffd859c35a6004e
laylowch <hao807775@gmail.com>
2026-09-14 11:52:01 +0800

    Reorder moe_sum import and update __all__ list
    Signed-off-by: laylowch <hao807775@gmail.com>

--- src/flaggems_vllm/runtime/backend/_hygon/fused/__init__.py
+++ src/flaggems_vllm/runtime/backend/_hygon/fused/__init__.py
@@ -17,15 +17,15 @@ from flaggems_vllm.runtime.backend._hygon.fused.fused_moe import (  # noqa: F401
     inplace_fused_experts,
     outplace_fused_experts,
 )
+from flaggems_vllm.runtime.backend._hygon.fused.moe_sum import moe_sum  # noqa: F401
 from flaggems_vllm.runtime.backend._hygon.fused.persistent_topk import (  # noqa: F401
     persistent_topk,
 )
-from flaggems_vllm.runtime.backend._hygon.fused.moe_sum import moe_sum  # noqa: F401
 
 __all__ = [
     "fused_experts_impl",
     "inplace_fused_experts",
-    "outplace_fused_experts",
     "moe_sum",
+    "outplace_fused_experts",
     "persistent_topk",
 ]
